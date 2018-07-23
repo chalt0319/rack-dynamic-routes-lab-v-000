01bd5b58a1ded3f3b32d2cb08bbf9a34bd2d0abb
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-23 19:03:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,3 +26,33 @@ class Application
   end
 
 end
+
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match("/items/")
+      item_name = req.path.split("/items/").last
+      
+      if !!@@items.find{|item| item.name == item_name}
+        @@items.each do |item|
+          if item.name == item_name
+          resp.write "#{item.price}"
+          resp.status = 200
+        end 
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
